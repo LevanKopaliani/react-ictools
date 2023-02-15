613e7d408b6de30d81c19144a364605ba1df1ccc
Levan Kopaliani <ciceron23x@gmail.com>
2023-02-16 00:15:16 +0400

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import { HiBars2 } from "react-icons/hi2";
@@ -8,6 +8,16 @@ import Logo from "../assets/img/Logo.png";
 import LogoText from "../assets/img/LogoText.png";
 import ToggleSwitch from "./ToggleSwitch/ToggleSwitch";
 import MainButton from "./MainButton";
+/// menu footer //
+import Address from "../assets/img/address.png";
+import Email from "../assets/img/email.png";
+import Phone from "../assets/img/phone.png";
+import fa from "../assets/img/fa.png";
+import tw from "../assets/img/tw.png";
+import ins from "../assets/img/ins.png";
+import lin from "../assets/img/lin.png";
+
+//////////////
 
 const NavBar = () => {
   let [checked, setChecked] = useState(false);
@@ -19,10 +29,15 @@ const NavBar = () => {
   const closeMobileMenu = () => {
     setMobileMenu(false);
   };
+  useEffect(() => {
+    const body = document.getElementById("navbar").parentNode;
+
+    mobileMenu ? (body.className = "menu-open") : (body.className = "");
+  }, [mobileMenu]);
 
   return (
     <IconContext.Provider value={{ color: "#29BEC8" }}>
-      <div className="navbar">
+      <div className="navbar" id="navbar">
         <div className="wrapper">
           <div className="navbar-container">
             <div className="nav-logo">
@@ -66,6 +81,56 @@ const NavBar = () => {
                   About Us
                 </NavLink>
               </li>
+              <div className="menu-footer">
+                <MainButton text={"request demo today"} type={"nav"} />
+                <div className="addressbar">
+                  <div className="contacts">
+                    <div className="head-office">
+                      <div className="icon">
+                        <img src={Address} alt="address" />
+                      </div>
+                      <div className="info">
+                        <p>Head office</p>
+                        <p className="head-office-short">
+                          Los Angeles, California
+                        </p>
+                        <p className="full-address">
+                          10817 Santa Monica Blvd
+                          <br />
+                          Ste. 310 , Los Angeles CA 90025
+                        </p>
+                      </div>
+                    </div>
+                    <div className="email">
+                      <div className="icon">
+                        <img src={Email} alt="address" />
+                      </div>
+                      <div className="info">
+                        <p>Email</p>
+                        <p>info@icvr.io</p>
+                      </div>
+                    </div>
+                    <div className="phone">
+                      <div className="icon">
+                        <img src={Phone} alt="address" />
+                      </div>
+                      <div className="info">
+                        <p>Phone</p>
+                        <p>+1 (310) 489-0185</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="social">
+                    <div className="social-links">
+                      <img src={fa} alt="Facebook" />
+                      <img src={tw} alt="Tweeter" />
+                      <img src={ins} alt="Instagram" />
+                      <img src={lin} alt="Linkedin" />
+                    </div>
+                    <MainButton type={"subscribe-button"} text="Subscribe" />
+                  </div>
+                </div>
+              </div>
             </ul>
             <div className="nav-buttons-container">
               <ToggleSwitch
